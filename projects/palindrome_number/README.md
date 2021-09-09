1d962a9403a05555386c869fa23654cde8157c7a
Sulav Maharjan <63336944+sulavmhrzn@users.noreply.github.com>
2021-09-09 11:18:10 +0545

    Updated main file. Added tests andREADME

--- projects/palindrome_number/README.md
+++ projects/palindrome_number/README.md
@@ -1,1 +1,12 @@
-This script checks if the given number is palindrome or not.
+# Palindrome Number Checker
+Palindrome Number Checker is a small python script that checks if the given number is a palindrome or not.
+
+# Tests
+```python
+python3 tests.py
+```
+
+# Run
+```python
+python3 main.py
+```
--- projects/palindrome_number/main.py
+++ projects/palindrome_number/main.py
@@ -1,7 +1,16 @@
-def palindrome(num):
+def palindrome(num: int) -> str:
+    """Given a number check if it is palindrome or not"""
+
+    if not isinstance(num, int):
+        raise TypeError("Please enter an integer")
+
     num = str(num)
-    return f'{num} is a palindrome' if (num[::-1]) == num else f'{num} is not a palindrome'
+
+    return (
+        f"{num} is a palindrome" if (num[::-1]) == num else f"{num} is not a palindrome"
+    )
 
 
-usr_inp = input('Enter a number or string to check if palindrome: ')
-print(palindrome(usr_inp))
+if __name__ == "__main__":
+    usr_inp = input("Enter a number or string to check if palindrome: ")
+    print(palindrome(usr_inp))
